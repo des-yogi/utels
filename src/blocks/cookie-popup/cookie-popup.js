8a4de0d346dc3f4328ee09cee77890ae167b5789
des-yogi <nuevo-design@mail.ru>
2021-07-27 20:05:36 +0300

    Старт проекта

---
+++ src/blocks/cookie-popup/cookie-popup.js
@@ -0,0 +1,20 @@
+(function() {
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#ffffff",
+        "text": "#020406"
+      },
+      "button": {
+        "background": "#1870b7",
+        "text": "#ffffff"
+      }
+    },
+    "position": "bottom-right",
+    "content": {
+      "message": "Этот сайт использует файлы cookie в соответствии с <a href='https://www.cookiesandyou.com/'>Политикой использования файлов cookie</a>. Если Вы согласны на использование нами файлов cookie, то нажмите кнопку «Принять».",
+      "dismiss": "Принять!",
+      "link": "Подробнее"
+    }
+  });
+})();
