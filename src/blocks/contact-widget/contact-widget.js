e1de9ddfb318e1a75af1038d8266c44b585b7ffb
des-yogi <nuevo-design@mail.ru>
2021-08-19 13:58:08 +0300

    Обновление стилей

--- src/blocks/contact-widget/contact-widget.js
+++ src/blocks/contact-widget/contact-widget.js
@@ -1,22 +1,22 @@
 // document.addEventListener('DOMContentLoaded', function(){});
-(function(){
-  const contactWidget = document.getElementById('contact-widget');
-  const elemNameArr = ['phone', 'envelope', 'telegram'];
-  if (!contactWidget) { return; }
-  const svgElem = contactWidget.querySelector('#widget-img');
-  let svgElemValue = svgElem.childNodes[0].href.baseVal;
-  let svgElemString = svgElemValue.split('#')[0];
-  let svgElemVar = svgElemValue.split('#')[1];
+//(function(){
+  // const contactWidget = document.getElementById('contact-widget');
+  // const elemNameArr = ['phone', 'envelope', 'telegram'];
+  // if (!contactWidget) { return; }
+  // const svgElem = contactWidget.querySelector('#widget-img');
+  // let svgElemValue = svgElem.childNodes[0].href.baseVal;
+  // let svgElemString = svgElemValue.split('#')[0];
+  // let svgElemVar = svgElemValue.split('#')[1];
   //console.info(svgElemString + '#' + svgElemVar);
 
-  svgElemValue = svgElemString + '#envelope';
+  //svgElemValue = svgElemString + '#envelope';
   //console.log(svgElemValue);
 
-  const changeImg = function () {
-    let count = 0;
-  };
+  // const changeImg = function () {
+    //let count = 0;
+  //};
 
   //setInterval(function () {
     //alert("Hello");
   //}, 3000);
-}());
+//}());
