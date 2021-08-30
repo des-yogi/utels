11c20ce23549bea1fe442f5f1b582ac253bd1619
des-yogi <nuevo-design@mail.ru>
2021-08-30 20:06:51 +0300

    Новый хедер с новым меню

--- src/blocks/main-nav/main-nav.js
+++ src/blocks/main-nav/main-nav.js
@@ -1,4 +1,4 @@
-(function (){
+/*(function (){
   const mainNav = document.getElementById('main-nav');
   const menuItemArr = mainNav.querySelectorAll('.main-nav__list>.main-nav__item');
   const activeClass = 'active';
@@ -11,4 +11,29 @@
       mainNav.classList.add('main-nav--bot-margin');
     }
   }
+})()*/
+
+(function (){
+  let new_scroll_position = 0;
+  let last_scroll_position;
+  const header = document.getElementById('header');
+
+  window.addEventListener('scroll', function(e) {
+    last_scroll_position = window.scrollY;
+
+    // Scrolling down
+    if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+      // header.removeClass('slideDown').addClass('slideUp');
+      header.classList.remove('page-header--slideDown');
+      header.classList.add('page-header--slideUp');
+
+    // Scrolling up
+    } else if (new_scroll_position > last_scroll_position) {
+      // header.removeClass('slideUp').addClass('slideDown');
+      header.classList.remove('page-header--slideUp');
+      header.classList.add('page-header--slideDown');
+    }
+
+    new_scroll_position = last_scroll_position;
+  });
 })()
