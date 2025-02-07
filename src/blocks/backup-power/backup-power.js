327eddb0c05eb60b1e8ac4386f96006e3417f5df
Igor <des.yogi@gmail.com>
2025-02-07 19:32:54 +0200

    backup power category

---
+++ src/blocks/backup-power/backup-power.js
@@ -0,0 +1,20 @@
+// (function(){
+//   //const utils = window.fizzyUIUtils;
+//   const backPow = document.querySelector('.power-card__wrapper');
+//   if (!backPow) { return; }
+
+//   const options = {
+//     cellAlign: 'left',
+//     contain: true,
+//     dragThreshold: 3,
+//     selectedAttraction: 0.01,
+//     friction: 0.15,
+//     //watchCSS: true,
+//     prevNextButtons: true,
+//     pageDots: false
+//   }
+
+//   new Flickity(backPow, options);
+// }());
+
+
--- src/blocks/photo-slider-popup/photo-slider-popup.js
+++ src/blocks/photo-slider-popup/photo-slider-popup.js
@@ -1,21 +1,22 @@
-(function() {
-  const photoSliderContainer = document.querySelector('.photo-slider-popup__slider');
-  //console.log(photoSliderContainer);
+document.addEventListener('DOMContentLoaded', () => {
+  const photoSliderContainers = document.querySelectorAll('.photo-slider-popup__slider');
 
-  if (!photoSliderContainer) { return; }
+  if (!photoSliderContainers.length) { return; }
 
   const options = {
     freeScroll: true,
     wrapAround: true,
-    //cellAlign: 'left',
-    //contain: true,
     dragThreshold: 3,
     selectedAttraction: 0.01,
     friction: 0.15,
-    //watchCSS: true,
+    //imagesLoaded: true,
     prevNextButtons: true,
     pageDots: false
-  }
+  };
 
-  const photoSlider = new Flickity(photoSliderContainer, options);
-}());
+  [...photoSliderContainers].forEach(item => {
+    imagesLoaded(item, function() {
+      new Flickity(item, options);
+    });
+  });
+});
